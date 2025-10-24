cb19a634e98c6f8ae6bc0539bb97de7f237e6b4b
Tommy <SUP3RS4IY4NX@gmail.com>
2025-10-23 22:51:43 -0400

    Made changes to the registration page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -27,7 +27,15 @@ export default function Login () {
   const input = "p-2 border border-gray-200 rounded"
   const inputSize = { width: "70%", height: "10%", marginBottom: "10px"}
 
-  const box = "flex flex-col items-center justify-center space-y-4 w-2/5 sm:w-2/3 md:w-2/5 h-1/2 border border-gray-300 rounded shadow-md bg-white/10 backdrop-blur-md"
+  const box = `
+  flex flex-col items-center justify-center space-y-4
+  w-11/12  
+  sm:w-2/3 
+  md:w-2/5
+  h-1/2
+  border border-gray-300 rounded shadow-md
+  bg-white/10 backdrop-blur-md
+`;
 
   const headerFormat={ marginBottom: "10px", fontSize: "200%"}
 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -2,9 +2,15 @@
 
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
+import { IoEyeSharp } from "react-icons/io5"
+import { FaEyeSlash } from "react-icons/fa"
+
+import { useState } from "react";
+
 
 export default function Register() {
   const { register, login } = useAuth();
+  const [ hidden, setHidden ] = useState(true);
   const router = useRouter();
 
   async function handleRegister(e: React.FormEvent) {
@@ -24,13 +30,85 @@ export default function Register() {
     }
   }
 
+  const container = "flex flex-col h-screen md:svh items-center justify-center"
+  const input = "p-2 border border-gray-200 rounded"
+  const inputSize = { width: "70%", height: "10%", marginBottom: "15px"}
+
+  const box = `
+  flex flex-col items-center justify-center space-y-4
+  w-11/12  
+  sm:w-2/3 
+  md:w-2/5
+  h-1/2
+  border border-gray-300 rounded shadow-md
+  bg-white/10 backdrop-blur-md
+`;
+
+const headerFormat={ marginBottom: "15px", fontSize: "200%"}
+
   return(
-    <form onSubmit={handleRegister}>
-      <input name="username" placeholder="Username" type="text" />
-      <input name="password" placeholder="Password" type="password" />
-      <input name="confirm_password" placeholder="Confirm Password" type="password" />
-      <input name="email" placeholder="Enter email here..." type="email" />
-      <button type="submit">Register</button>
-    </form>
+    <div className={container}>
+      <form onSubmit={handleRegister} className={box}>
+        <h2 className="font-bold" style={ headerFormat }>Register</h2>
+        <input name="username" placeholder="Username" type="text" className={input} style={inputSize} />
+        <input name="email" placeholder="Enter email here..." type="email" className={input} style={inputSize} />
+        <div style={{ position: "relative", width: "70%", marginBottom: "15px" }}>
+            <input 
+              name="password" 
+              placeholder="Password" 
+              type={ hidden ? "password": "text"} 
+              className={input} 
+              style={{ paddingRight: "2.5rem", width:"100%"}}/>
+              <button
+                type="button"
+                onClick={() => setHidden(!hidden)}
+                style={{
+                  position: "absolute",
+                  right: "0.5rem",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                  background: "transparent",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                >
+                  {hidden ? (
+                      <IoEyeSharp />
+                  ): (
+                    <FaEyeSlash />
+                  )}
+              </button>
+          </div>
+          <div style={{ position: "relative", width: "70%", marginBottom: "15px" }}>
+            <input 
+              name="confirm_password" 
+              placeholder="Confirm Password" 
+              type={ hidden ? "password": "text"} 
+              className={input} 
+              style={{ paddingRight: "2.5rem", width:"100%"}}/>
+              <button
+                type="button"
+                onClick={() => setHidden(!hidden)}
+                style={{
+                  position: "absolute",
+                  right: "0.5rem",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                  background: "transparent",
+                  border: "none",
+                  cursor: "pointer",
+                }}
+                >
+                  {hidden ? (
+                      <IoEyeSharp />
+                  ): (
+                    <FaEyeSlash />
+                  )}
+              </button>
+          </div>
+        <button type="submit" className={input + " text-black cursor-pointer bg-gray-200 hover:bg-blue-700"} style={inputSize}>Register</button>
+      </form>
+    </div>
+    
   )
 }
